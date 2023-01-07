8a267c57e0f77e4e0d4fae6dbd51646f1a3d89ff
ethanwhitcher <ewhitcher123@gmail.com>
2023-01-07 01:23:06 -0500

    made expenses have dynamic output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,29 +13,18 @@ const Expenses = (props) => {
 
     return (
         <div>
-        <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} onYearChange={yearChangeHandler}/>
-            <ExpenseItem 
-                title={props.items[0].title} 
-                amount={props.items[0].amount} 
-                date={props.items[0].date}
-            />
-            <ExpenseItem 
-                title={props.items[1].title} 
-                amount={props.items[1].amount} 
-                date={props.items[1].date}
-            />
-            <ExpenseItem 
-                title={props.items[2].title} 
-                amount={props.items[2].amount} 
-                date={props.items[2].date}
-            />
-            <ExpenseItem 
-                title={props.items[3].title} 
-                amount={props.items[3].amount} 
-                date={props.items[3].date}
-            />
-        </Card>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onYearChange={yearChangeHandler}/>
+                {/* Maps each expense object passed through props to jsx element*/}
+                {props.items.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date} 
+                    />
+                ))}
+            </Card>
         </div>
     );
 }
