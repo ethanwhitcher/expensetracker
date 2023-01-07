6b9601fc0728df30a5637dc219c73a8bad92fc9a
ethanwhitcher <ewhitcher123@gmail.com>
2023-01-07 13:04:17 -0500

    new component ExpensesList

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -15,25 +15,12 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date} 
-            />
-        ));
-    }
-
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onYearChange={yearChangeHandler}/>
                 {/* Maps each expense object passed through props to jsx element*/}
-                {expensesContent}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     );
