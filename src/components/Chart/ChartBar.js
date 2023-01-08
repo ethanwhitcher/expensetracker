da2604ff2de6950c254e86a23bac32fb0b123c37
ethanwhitcher <ewhitcher123@gmail.com>
2023-01-08 17:33:21 -0500

    chart bars

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ChartBar from './ChartBar';
+
+const Chart = (props) => {
+    return (
+        <div className='chart-bar'>
+            <div className='chart-bar__inner'>
+                <div className='chart-bar__fill'>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Chart;
