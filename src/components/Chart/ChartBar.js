b899fa71f4143d319d9c568f9722ffe2e8c62c10
ethanwhitcher <ewhitcher123@gmail.com>
2023-02-18 23:21:31 -0500

    added chartbar logic

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import ChartBar from './ChartBar';
+import './ChartBar.css';
 
 const Chart = (props) => {
+    let barFillHeight = '0%';
+
+    if (props.maxValue > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
     return (
         <div className='chart-bar'>
             <div className='chart-bar__inner'>
-                <div className='chart-bar__fill'>
-                    
-                </div>
+                <div className='chart-bar__fill' style={{height: barFillHeight}}></div>
             </div>
+            <div className='char-bar__label'>{props.label}</div>
         </div>
     );
 }
